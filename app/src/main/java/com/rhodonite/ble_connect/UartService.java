e4eae5ac30c958360a1c8ac13b14d279029edd22
Rhodonite <rhodonite@hotmail.com>
2022-01-27 15:34:44 +0800

    Update UartService.java

--- app/src/main/java/com/rhodonite/ble_connect/UartService.java
+++ app/src/main/java/com/rhodonite/ble_connect/UartService.java
@@ -1,25 +1,4 @@
 
-/*
- * Copyright (c) 2015, Nordic Semiconductor
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution.
- *
- * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this
- * software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
- * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package com.rhodonite.ble_connect;
 
 import android.annotation.TargetApi;
@@ -79,9 +58,9 @@ public class UartService extends Service {
     public final static String DEVICE_DOES_NOT_SUPPORT_UART =
             "com.rhodonite.DEVICE_DOES_NOT_SUPPORT_UART";
 
-    public static final UUID RX_SERVICE_UUID = UUID.fromString("4157a00a-D129-AEB5-859B-D547E30FFCB1");
-    public static final UUID TX_CHAR_UUID = UUID.fromString("4157B511-D129-AEB5-859B-D547E30FFCB1");//write
-    public static final UUID RX_CHAR_UUID = UUID.fromString("4157B512-D129-AEB5-859B-D547E30FFCB1");//read
+    public static final UUID RX_SERVICE_UUID = UUID.fromString("");//輸入UUID 例如:a00a
+    public static final UUID TX_CHAR_UUID = UUID.fromString("");//write 例如:b511
+    public static final UUID RX_CHAR_UUID = UUID.fromString("");//read  例如:512
 
 
     // Implements callback methods for GATT events that the app cares about.  For example,
